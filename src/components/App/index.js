88bc8b072800a95cb553223786388c47a1877568
Łukasz Szpil <cheatoge@gmail.com>
2020-01-12 19:51:29 +0100

    Tweak structure

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,1 @@
-import React from 'react'
-import './App.css'
-
-function App() {
-  return (
-    <div className="App">
-      <span>Test</span>
-    </div>
-  )
-}
-
-export default App
+export { App } from './App'
