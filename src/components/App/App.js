602c38cccbd3f3a893f491ed72b978a979934157
Łukasz Szpil <cheatoge@gmail.com>
2020-01-12 22:48:03 +0100

    Move test data to separate file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,75 +1,14 @@
 import React from 'react'
 import './App.css'
 import { ContactList } from '../ContactList'
+import { contacts } from '../../util/TestData'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: 'Saul Goodman',
-          phones: [
-            {
-              number: '600600600',
-              isBusiness: false
-            }
-          ]
-        },
-        {
-          id: 2,
-          name: 'Mike Tyson',
-          phones: [
-            {
-              number: '333343444'
-            },
-            {
-              number: '636263634',
-              isBusiness: true
-            }
-          ]
-        },
-        {
-          id: 1,
-          name: 'Sandra May',
-          phones: [
-            {
-              number: '323602410'
-            },
-            {
-              number: '323602410'
-            }
-          ]
-        },
-        {
-          id: 2,
-          name: 'Paul Walker',
-          phones: [
-            {
-              number: '434345333',
-              isBusiness: true
-            },
-            {
-              number: '868444333',
-              isBusiness: true
-            }
-          ]
-        },
-        {
-          id: 2,
-          name: 'Harry Potter',
-          phones: [
-            {
-              number: '333444555'
-            },
-            {
-              number: '666778889'
-            }
-          ]
-        }
-      ]
+      contacts: contacts
     }
   }
 
---
+++ src/util/TestData.js
@@ -0,0 +1,65 @@
+const contacts = [
+  {
+    id: 1,
+    name: 'Saul Goodman',
+    phones: [
+      {
+        number: '600600600',
+        isBusiness: false
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Mike Tyson',
+    phones: [
+      {
+        number: '333343444'
+      },
+      {
+        number: '636263634',
+        isBusiness: true
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: 'Sandra May',
+    phones: [
+      {
+        number: '323602410'
+      },
+      {
+        number: '545433322'
+      }
+    ]
+  },
+  {
+    id: 4,
+    name: 'Paul Walker',
+    phones: [
+      {
+        number: '434345333',
+        isBusiness: true
+      },
+      {
+        number: '868444333',
+        isBusiness: true
+      }
+    ]
+  },
+  {
+    id: 5,
+    name: 'Harry Potter',
+    phones: [
+      {
+        number: '333444555'
+      },
+      {
+        number: '666778889'
+      }
+    ]
+  }
+]
+
+export { contacts }
